3ce0ec3af221acb339edaf462cdf92af1d9b5645
jan-far <ligalifarouk@gmail.com>
2020-07-28 16:46:37 +0100

    Update body.js

--- asset/js/body.js
+++ asset/js/body.js
@@ -1,41 +1,46 @@
 var body = document.querySelector("body");
 
-window.addEventListener("load", ()=>{
-        body.innerHTML = `
+const PlayerOne = () => {
+    body.innerHTML = `
     <h1>Player One</h1>
-    <input type="text" id="input1" placeholder="Github Username">
-    <button class="proceed1">Continue</button>
-    `
+        <input type="text" id="input1" placeholder="Github Username">
+        <button type="submit" class="proceed1">Continue</button>
+        `
+    var proceed1 = document.querySelector(".proceed1");
+    return proceed1;
+}
 
-    var  proceed1 = document.querySelector(".proceed1");
-    proceed1.addEventListener("click", ()=>{
-        const input1 = document.querySelector("#input1").value;
-
-        if(input1 == null || ` `){
-            alert("Please enter a github username!");
-        }else{
-            console.log(input1)
-            // user1 = getUser(input1);
-            body.innerHTML = `
+const PlayerTwo = () => {
+    body.innerHTML = `
             <h1>Player  Two</h1>
-            <input type="text" id="input2" placeholder="Github Username">
-            <button class="proceed2">Continue</button>
-            `;
+                <input id="input2" placeholder="Github Username">
+                <button type="submit" class="proceed2">Continue</button>
+            `
+    var proceed2 = document.querySelector(".proceed2")
+    return proceed2;
+}
 
-            var proceed2 = document.querySelector(".proceed2");
-            proceed2.addEventListener("click", ()=>{
-            var input2 = document.querySelector("#input2").value;
-            user2 = getUser(input2);
+const play = () => {
+    PlayerOne().addEventListener("click", () => {
+        var input1 = document.querySelector("#input1").value;
 
-            if(input2 == null || ` `){
-                alert("Please enter a github username!");
-            }else{
-            console.log (user1, user2)}
-        })
+        if (input1 == "") {
+            alert("Please enter a github username!");
         }
-        
+        else {
+            PlayerTwo().addEventListener("click", () => {
+                var input2 = document.querySelector("#input2").value;
 
-
-
+                if (input2 == "") {
+                    alert("Please enter a github username!");
+                } else {
+                    localStorage.setItem("input1", input1)
+                    localStorage.setItem("input2", input2)
+                    window.open("../../UI/displayUsers.html", "_self");
+                }
+            })
+        }
     })
-})
+};
+
+play();
