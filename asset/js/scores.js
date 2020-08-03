08e5ee9d48e54de690c7043a5880953e24f816bb
jan-far <ligalifarouk@gmail.com>
2020-08-03 15:14:27 +0100

    Update scores.js

--- asset/js/scores.js
+++ asset/js/scores.js
@@ -2,17 +2,17 @@ import User from "../../API/fetch.js";
 
 var input1 = localStorage.getItem("input1");
 var input2 = localStorage.getItem("input2");
-let body = document.querySelector("body");
 
-const calculate = {
-    calculateScores(user) {
-        User.getUser(user)
-            .then(data => {
-                data.followers + data.following + data.public_repos
-            })
-            .catch((e) => {
-                return e
-            })
-    }
+const calculate = async () => {
+    let in1 = User.getUser(input1)
+        .then(data => {
+            sessionStorage.setItem("in1", data.followers + data.following + data.public_repos)
+        })
+
+    let in2 = User.getUser(input2)
+        .then(data => {
+            sessionStorage.setItem("in2", data.followers + data.following + data.public_repos) 
+        })
 }
+
 export default calculate;
